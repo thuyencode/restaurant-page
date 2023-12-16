06685d392a7178ba6cdbfd62c367dfee3e2011d0
thuyencode <thuyencode@proton.me>
2023-12-16 23:10:19 +0700

    Many changes
    Add new serif font, complete menu styles, add menu items using JavaScript

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
         xs: '475px'
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Playfair Display', ...defaultTheme.fontFamily.serif]
+      },
+      backgroundImage: {
+        menu: "url('https://plus.unsplash.com/premium_photo-1694715915615-51472fccf407?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       }
     }
   },
