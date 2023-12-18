1b337006203b4f864a9e887dfbf12f06ce79e365
thuyencode <thuyencode@proton.me>
2023-12-18 16:30:17 +0700

    Change background images' size

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
       backgroundImage: {
         menu: "url('https://plus.unsplash.com/premium_photo-1694715915615-51472fccf407?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         'home-dark':
-          "url('https://images.unsplash.com/photo-1672305330859-71321689e159?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=leohoho-tj9tKXXs0cU-unsplash.jpg&w=1920')",
-        home: "url('https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=petr-sevcovic-qE1jxYXiwOA-unsplash.jpg&w=1920')"
+          "url('https://images.unsplash.com/photo-1672305330907-8092be9161ea?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=leohoho-uskQ8hyZNgQ-unsplash.jpg&w=640')",
+        home: "url('https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=petr-sevcovic-qE1jxYXiwOA-unsplash.jpg&w=640')"
       }
     }
   },
